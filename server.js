c62daf64fd7839b855875b94c7a077a39bfa0f56
developermodoki <61893884+developermodoki@users.noreply.github.com>
2022-02-08 09:46:39 +0000

    Update deploy to output generated at 3e702ce

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const discord_js_1 = require("discord.js");
 const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.DIRECT_MESSAGES, discord_js_1.Intents.FLAGS.GUILD_MESSAGES] });
 client.on("ready", () => {
