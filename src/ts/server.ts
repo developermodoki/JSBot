d04798110a93484fde3a6644c886d6d2ef8556b2
developermodoki <61893884+developermodoki@users.noreply.github.com>
2022-02-08 19:50:44 +0900

    Update

--- src/ts/server.ts
+++ src/ts/server.ts
@@ -1,11 +1,4 @@
 import { Message,Client,Intents } from "discord.js";
-import express, { Request,Response } from "express";
-
-const app: express.Express = express();
-app.get("/",(req:Request, res:Response) => {
-    res.send("Not found");
-});
-app.listen(process.env.PORT || 5000);
 
 const client = new Client({intents:[Intents.FLAGS.GUILDS,Intents.FLAGS.DIRECT_MESSAGES,Intents.FLAGS.GUILD_MESSAGES]});
 
