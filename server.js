17933098b00b04dc2a349d1107ec0395a1cc70f6
developermodoki <61893884+developermodoki@users.noreply.github.com>
2022-02-11 05:23:45 +0000

    Update deploy to output generated at f8376bd

--- server.js
+++ server.js
@@ -5,20 +5,25 @@ const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GU
 client.on("ready", () => {
     console.log("This Bot is ready");
 });
-// message
+// messages
 client.on("messageCreate", (message) => {
+    var _a, _b;
     console.log("MESSAGE CREATED");
     if (message.author.bot)
         return;
-    if (message.content === "js" || message.content === "JS" || message.content === "JavaScript" || message.content === "javascript") {
-        message.reply("Yeah! JavaScript is very very AMAZING!!!!!!");
+    if ((_b = (_a = message.guild) === null || _a === void 0 ? void 0 : _a.me) === null || _b === void 0 ? void 0 : _b.permissions.has("SEND_MESSAGES")) {
+        if (message.content === "js" || message.content === "JS" || message.content === "JavaScript" || message.content === "javascript") {
+            message.reply("Yeah! JavaScript is very very AMAZING!!!!!!");
+        }
+        else if (message.content.match(/js|JS|JavaScript|javascript/)) {
+            message.react(process.env.JS_EMOJI_ID);
+        }
+        if (message.content === "java" || message.content === "Java") {
+            message.reply("My big brother Java!!!!!!!");
+        }
     }
-    else if (message.content.match(/js|JS|JavaScript|javascript/)) {
-        //message.reply("JS");
-        message.react(process.env.JS_EMOJI_ID);
-    }
-    if (message.content === "java" || message.content === "Java") {
-        message.reply("My big brother Java!!!!!!!!!!!!!!!!!");
+    else {
+        return;
     }
 });
 client.login(process.env.DISCORD_TOKEN);
