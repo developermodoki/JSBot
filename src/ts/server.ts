f8376bdf04e882f7bc428607a724c24c01d9e7b2
developermodoki <61893884+developermodoki@users.noreply.github.com>
2022-02-11 14:23:20 +0900

    handling permissions

--- src/ts/server.ts
+++ src/ts/server.ts
@@ -6,18 +6,21 @@ client.on("ready",() => {
     console.log("This Bot is ready");
 });
 
-// message
+// messages
 client.on("messageCreate",(message:Message) => {
     console.log("MESSAGE CREATED")
     if (message.author.bot) return;
-    if(message.content === "js" || message.content === "JS" || message.content === "JavaScript" || message.content === "javascript") {
-        message.reply("Yeah! JavaScript is very very AMAZING!!!!!!")
-    } else if(message.content.match(/js|JS|JavaScript|javascript/)) {
-        //message.reply("JS");
-        message.react(process.env.JS_EMOJI_ID as string);
-    } 
-    if(message.content === "java" || message.content === "Java") {
-        message.reply("My big brother Java!!!!!!!!!!!!!!!!!");
+    if (message.guild?.me?.permissions.has("SEND_MESSAGES")){
+        if(message.content === "js" || message.content === "JS" || message.content === "JavaScript" || message.content === "javascript"){
+            message.reply("Yeah! JavaScript is very very AMAZING!!!!!!");
+        } else if(message.content.match(/js|JS|JavaScript|javascript/)) {
+            message.react(process.env.JS_EMOJI_ID as string);
+        }
+        if(message.content === "java" || message.content === "Java") {
+            message.reply("My big brother Java!!!!!!!")
+        }
+    } else {
+        return;
     }
 });
 
