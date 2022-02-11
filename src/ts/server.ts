e6435188675ab8a8782ab417fcfff8a68b3b6ea9
developermodoki <61893884+developermodoki@users.noreply.github.com>
2022-02-11 12:49:46 +0900

    fixed(2)

--- src/ts/server.ts
+++ src/ts/server.ts
@@ -1,4 +1,4 @@
-import { Message,Client,Intents,process } from "discord.js";
+import { Message,Client,Intents } from "discord.js";
 
 const client = new Client({intents:[Intents.FLAGS.GUILDS,Intents.FLAGS.DIRECT_MESSAGES,Intents.FLAGS.GUILD_MESSAGES]});
 
@@ -20,6 +20,7 @@ client.on("messageCreate",(message:Message) => {
         message.reply("My big brother Java!!!!!!!!!!!!!!!!!");
     }
 });
+
 
 
 client.login(process.env.DISCORD_TOKEN);
