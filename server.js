e545ebca63abe4b7a42d65653f41db0f707fb3c4
developermodoki <61893884+developermodoki@users.noreply.github.com>
2022-02-14 13:49:46 +0000

    Update deploy to output generated at b3766ff

--- server.js
+++ server.js
@@ -1,48 +1,73 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const builders_1 = require("@discordjs/builders");
 const rest_1 = require("@discordjs/rest");
+const v9_1 = require("discord-api-types/v9");
+const vm = __importStar(require("vm"));
 const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.DIRECT_MESSAGES, discord_js_1.Intents.FLAGS.GUILD_MESSAGES] });
 const commands = [
-    new builders_1.SlashCommandBuilder().setName("js").setDescription("To run JavaScript's code").addStringOption(opt => opt.setName("code")),
-    new builders_1.SlashCommandBuilder().setName("searchstack").setDescription("To search Stack Overflow").addStringOption(opt => opt.setName("word")),
-    new builders_1.SlashCommandBuilder().setName("searchmdn").setDescription("To search MDN").addStringOption(opt => opt.setName("word"))
+    new builders_1.SlashCommandBuilder().setName("runjs").setDescription("To run JavaScript's code").addStringOption(opt => opt.setName("code")),
+    new builders_1.SlashCommandBuilder().setName("searchstack").setDescription("To search Stack Overflow").addStringOption(opt => opt.setName("stackWord")),
+    new builders_1.SlashCommandBuilder().setName("searchmdn").setDescription("To search MDN").addStringOption(opt => opt.setName("mdnWord"))
 ];
 const rest = new rest_1.REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 client.on("ready", () => {
     console.log("This Bot is ready");
 });
-/*
-client.on("guildCreate",guild => {
-    rest.put(Routes.applicationGuildCommands(process.env.BOT_ID as string, guild.id.toString()), {body:commands})
+client.on("guildCreate", guild => {
+    rest.put(v9_1.Routes.applicationGuildCommands(process.env.BOT_ID, guild.id.toString()), { body: commands })
         .then(() => console.log("Registred commands"))
-        .catch(error => console.log("Failed registred commands"))
-})
-
-client.on("interactionCreate",inter => {
-    if(!inter.isCommand()) return;
-    if(inter.commandName === "js") {
-        const optStr = inter.options.getString;
+        .catch(error => console.log("Failed registred commands"));
+});
+client.on("interactionCreate", inter => {
+    if (!inter.isCommand())
+        return;
+    if (inter.commandName === "js") {
+        if (inter.options.getString("code") === null)
+            return;
+        const optStr = inter.options.getString("code");
         const context = vm.createContext();
         vm.runInContext(`(outer) => {
             globalThis.console = {
              log(...args) {
              outer.console.log(...args);
             }
-          };
-        }`,context)({console});
+          };getString
+        }`, context)({ console });
         try {
-            vm.runInContext(optStr as unknown as string,context);
-        } catch(e) {
-            
+            inter.reply((0, builders_1.codeBlock)("js", vm.runInContext(optStr, context)));
+        }
+        catch (e) {
+            inter.reply((0, builders_1.codeBlock)("js", e));
         }
     }
+    if (inter.commandName === "searchstack") {
+        (inter.options.getString("stackWord") !== null) ? inter.reply(`https://stackoverflow.com/search?q=${inter.options.getString("stackWord")}`) : void 0;
+    }
+    if (inter.commandName === "searchmdn") {
+        (inter.options.getString("mdnWord") !== null) ? inter.reply(`https://https://developer.mozilla.org/ja/search?q=${inter.options.getString("mdnWord")}`) : void 0;
+    }
 });
-
-IT'S DISABLED
-BECAUSE IT'S NOT DOING ENOUGH TESTING.
-*/
 // messages
 client.on("messageCreate", (message) => {
     var _a, _b, _c, _d;
@@ -67,7 +92,7 @@ client.on("messageCreate", (message) => {
             }
         }
         else if (message.content.match(/js|JS|JavaScript|javascript/)) {
-            findJSemoji ? message.react(findJSemoji.toString()) : (findJSemoji2 ? message.react(findJSemoji2.toString()) : void (0));
+            findJSemoji ? message.react(findJSemoji.toString()) : (findJSemoji2 ? message.react(findJSemoji2.toString()) : void 0);
             try {
                 console.log("test");
             }
