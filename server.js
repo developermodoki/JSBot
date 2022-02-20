2781978dea62f553597405f42254a3200e64610b
developermodoki <61893884+developermodoki@users.noreply.github.com>
2022-02-20 06:02:51 +0000

    Update deploy to output generated at f08014d

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const builders_1 = require("@discordjs/builders");
 const rest_1 = require("@discordjs/rest");
 const v9_1 = require("discord-api-types/v9");
+const axios_1 = __importDefault(require("axios"));
 const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.DIRECT_MESSAGES, discord_js_1.Intents.FLAGS.GUILD_MESSAGES] });
+;
 const commands = [
     new builders_1.SlashCommandBuilder().setName("runjs").setDescription("Run JavaScript's code").addStringOption(opt => opt.setName("code").setDescription("Program Code").setRequired(true)),
     new builders_1.SlashCommandBuilder().setName("searchstack").setDescription("Sarch Stack Overflow").addStringOption(opt => opt.setName("stackword").setDescription("word of Stack Overflow").setRequired(true)),
@@ -23,19 +37,49 @@ client.on("guildCreate", guild => {
         .then(() => console.log("Registred commands"))
         .catch(error => console.log(error));
 });
-client.on("interactionCreate", inter => {
+client.on("interactionCreate", (inter) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c;
     if (!inter.isCommand())
         return;
     if (inter.commandName === "runjs") {
-        inter.reply("This feature is under development");
+        yield ((_a = inter.channel) === null || _a === void 0 ? void 0 : _a.send("This feature is under development"));
     }
     if (inter.commandName === "searchstack") {
-        (inter.options.getString("stackword") !== null) ? inter.reply(`https://stackoverflow.com/search?q=${inter.options.getString("stackword")}`) : void 0;
+        (inter.options.getString("stackword") !== null) ? yield inter.reply(`https://stackoverflow.com/search?q=${inter.options.getString("stackword")}`) : void 0;
     }
     if (inter.commandName === "searchmdn") {
-        (inter.options.getString("mdnword") !== null) ? inter.reply(`https://developer.mozilla.org/ja/search?q=${inter.options.getString("mdnword")}`) : void 0;
+        let Success = false;
+        const mdnApiResponse = yield axios_1.default.get(`https://developer.mozilla.org/api/v1/search?q=${inter.options.getString("mdnword")}&locale=ja`);
+        for (let i of mdnApiResponse.data.document) {
+            if (i.title === inter.options.getString("mdnword")) {
+                yield ((_b = inter.channel) === null || _b === void 0 ? void 0 : _b.send({ embeds: [{
+                            color: 16776960,
+                            title: "Result",
+                            fields: [
+                                {
+                                    name: i.title,
+                                    value: i.summary
+                                }
+                            ]
+                        }] }));
+                Success = true;
+                break;
+            }
+            if (!Success) {
+                yield ((_c = inter.channel) === null || _c === void 0 ? void 0 : _c.send({ embeds: [{
+                            color: 16776960,
+                            title: "Result",
+                            fields: [
+                                {
+                                    name: "Not Found",
+                                    value: "not exist"
+                                }
+                            ]
+                        }] }));
+            }
+        }
     }
-});
+}));
 // messages
 client.on("messageCreate", (message) => {
     var _a, _b, _c, _d;
